69665b5b4fcf51b839d573a2e659b9c099d59ea7
daria-marmuzevich <78801833+daria-marmuzevich@users.noreply.github.com>
2021-04-04 19:40:20 +0300

    Add files via upload

---
+++ Алгоритмы: теория и практика. Методы/последняя цифра большого числа Фибоначчи.cpp
@@ -0,0 +1,31 @@
+﻿/*Дано число 1≤n≤10^7, необходимо найти последнюю цифру n-го числа Фибоначчи.*/
+
+#include <cassert>
+#include <iostream>
+
+class Fibonacci {
+public:
+    static int get_last_digit(int n) {
+        assert(n >= 1);
+        int a=0, b=1;
+        for (int i=2; i < n; i++)
+        {
+            if(i%2==0)
+            a = (a + b)%10;
+            else 
+            b = (b + a)%10;
+        }
+
+        n = (a + b) % 10;
+        
+        return n;
+    }
+};
+
+int main(void) {
+    int n;
+    std::cin >> n;
+    std::cout << Fibonacci::get_last_digit(n) << std::endl;
+    system("pause");
+    return 0;
+}
